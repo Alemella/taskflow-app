64f1854d9efdb48eedc7de7d1283fe029a1385a8
Alemella <alejandros.mella5@gmail.com>
2026-07-13 17:33:39 -0400

    fix docker

--- backend/src/main/java/taskflow_backend/service/JwtService.java
+++ backend/src/main/java/taskflow_backend/service/JwtService.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -12,8 +14,8 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY = "mi_clave_secreta_super_segura_taskflow_2026";
-
+    @Value("${TASKFLOW_JWT_SECRET:mi_clave_secreta_super_segura_taskflow_2026}")
+    private String SECRET_KEY;
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
